cde4b6010ce2077c1a8c09592d6e2d58126a452e
mahdi <mhdrslzdh1383@gmail.com>
2024-01-04 22:29:01 +0330

    input, select, validation structure

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,3 +1,16 @@
 <script>
-    import "../app.css";
+	import '../app.css';
+	import '@flaticon/flaticon-uicons/css/regular/rounded.css';
+	import '@flaticon/flaticon-uicons/css/solid/rounded.css';
 </script>
+
+<main>
+	<slot />
+</main>
+
+<style>
+	main {
+		background-color: #292929;
+		min-height: 100vh;
+	}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				'-gray': '#292929',
+				'-error': '#F17373',
+				'-success': '#5AE4BB'
+			},
+			fontFamily: {
+				iransans: ['iransans'],
+				fedra: ['fedra']
+			},
+			maxWidth: {
+				'100vw': '100vw'
+			}
+		}
+	},
+	plugins: [require('daisyui')]
+};
