44da209978c329c001c037a413e8eb96a41037fc
Parsa Torbati <ptorbatii@gmail.com>
2024-01-06 00:42:24 +0330

    degrees form and logic done

--- src/js/index.js
+++ src/js/index.js
@@ -50,7 +50,6 @@ function addFieldInput() {
     var lastIdx = fs.dataset["lastIdx"];
     var curIdx = parseInt(lastIdx) + 1;
     fs.dataset["lastIdx"] = curIdx;
-//    var el = document.getElementById("field-" + lastIdx);
     var newEl = document.createElement("div");
 
     var newElI = document.createElement("input");
@@ -83,8 +82,115 @@ function removeFieldInput(idx) {
     el.parentElement.removeChild(el);
 }
 
+function addDegreeInput() {
+    var fs = document.getElementById("degrees");
+    var lastIdx = fs.dataset["lastIdx"];
+    var curIdx = parseInt(lastIdx) + 1;
+    fs.dataset["lastIdx"] = curIdx;
+
+    var d = document.createElement("div");
+    {
+    var s1 = document.createElement("select");
+    s1.name = "degree-" + curIdx + "-type";
+    s1.id = "degree-" + curIdx + "-type";
+    s1.required = true;
+    {
+        var o1 = document.createElement("option"); o1.value = "BACHELOR"; o1.innerText = "کارشناسی"; s1.options.add(o1);
+        var o2 = document.createElement("option"); o2.value = "MASTER"; o2.innerText = "کارشناسی ارشد"; s1.options.add(o2);
+        var o3 = document.createElement("option"); o3.value = "PHD"; o3.innerText = "دکترا"; s1.options.add(o3);
+        var o4 = document.createElement("option"); o4.value = "POSTDOC"; o4.innerText = "پسادکترا"; s1.options.add(o4);
+    }
+    d.appendChild(s1);
+    }
+    {
+    var l2 = document.createElement("label");
+    l2.setAttribute("for", "degree-" + curIdx + "-uni");
+    l2.innerText = "دانشگاه";
+    var i2 = document.createElement("input");
+    i2.type = "text";
+    i2.name = "degree-" + curIdx + "-uni";
+    i2.id = "degree-" + curIdx + "-uni";
+    i2.dataset["completionType"] = "degrees/uni";
+    i2.required = true;
+    i2.setAttribute("oninput", "invokeCompletion()");
+    i2.setAttribute("list", "degree-" + curIdx + "-uni-dl");
+    var dl2 = document.createElement("datalist");
+    dl2.id = "degree-" + curIdx + "-uni-dl";
+
+    d.appendChild(l2);
+    d.appendChild(i2);
+    d.appendChild(dl2);
+    }
+    {
+    var l3 = document.createElement("label");
+    l3.setAttribute("for", "degree-" + curIdx + "-major");
+    l3.innerText = "رشته";
+    var i3 = document.createElement("input");
+    i3.type = "text";
+    i3.name = "degree-" + curIdx + "-major";
+    i3.id = "degree-" + curIdx + "-major";
+    i3.dataset["completionType"] = "degrees/major";
+    i3.required = true;
+    i3.setAttribute("oninput", "invokeCompletion()");
+    i3.setAttribute("list", "degree-" + curIdx + "-major-dl");
+    var dl3 = document.createElement("datalist");
+    dl3.id = "degree-" + curIdx + "-major-dl";
+
+    d.appendChild(l3);
+    d.appendChild(i3);
+    d.appendChild(dl3);
+    }
+    {
+    var l4 = document.createElement("label");
+    l4.setAttribute("for", "degree-" + curIdx + "-start");
+    l4.innerText = "سال شروع";
+    var i4 = document.createElement("input");
+    i4.type = "text";
+    i4.name = "degree-" + curIdx + "-start";
+    i4.id = "degree-" + curIdx + "-start";
+    i4.required = true;
+    i4.minLength = 4;
+    i4.maxLength = 4;
+    i4.setAttribute("inputmode", "number");
+
+    d.appendChild(l4);
+    d.appendChild(i4);
+    }
+    {
+    var l5 = document.createElement("label");
+    l5.setAttribute("for", "degree-" + curIdx + "-end");
+    l5.innerText = "سال پایان (احتمالی)";
+    var i5 = document.createElement("input");
+    i5.type = "text";
+    i5.name = "degree-" + curIdx + "-end";
+    i5.id = "degree-" + curIdx + "-end";
+    i5.required = true;
+    i5.minLength = 4;
+    i5.maxLength = 4;
+    i5.setAttribute("inputmode", "number");
+
+    d.appendChild(l5);
+    d.appendChild(i5);
+    }
+    {
+    var sp = document.createElement("span");
+    sp.classList.add("pointer");
+    sp.classList.add("minus-button");
+    sp.setAttribute("onclick", "removeDegreeInput(" + curIdx + ")");
+    sp.innerText = "حذف";
+    d.appendChild(sp);
+    }
+    document.getElementById("degree-add").before(d);
+}
+
+function removeDegreeInput(idx) {
+    var el = document.getElementById("degree-" + idx + "-type").parentElement;
+    el.parentElement.removeChild(el);
+}
+
 function onLoad() {
     addFieldInput();
+    addDegreeInput();
 }
 
 onLoad();
