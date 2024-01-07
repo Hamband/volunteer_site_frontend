ca6e35c9078624101f3d9c016177b9d21cb849ec
Parsa Torbati <ptorbatii@gmail.com>
2024-01-08 02:03:29 +0330

    add form for deleting

---
+++ src/js/delete.js
@@ -0,0 +1,95 @@
+let baseUrl = "https://hamband.math.sharif.edu/volunteer/api/v2";
+let key = "mecaenizajocjutebyeckrewtaegckor";
+
+function performDeleteRequest(url, params) {
+    url = baseUrl + url;
+    params["api_key"] = key;
+    var target = new URL(url);
+    target.search = new URLSearchParams(params).toString();
+
+    return new Promise(function (resolve, reject) {
+        var xhttp = new XMLHttpRequest();
+        xhttp.open("DELETE", target, true);
+        xhttp.responseType = "json";
+        xhttp.onload = function() {
+            if (xhttp.status == 200) {
+                resolve(xhttp.response);
+            }
+            else {
+                reject(xhttp.status);
+            }
+        };
+        xhttp.send();
+    });
+}
+
+function getSearchParam(key) {
+    if (window.location.search == "" || window.location.search == "?") {
+        return null;
+    }
+    var list = window.location.search.split("?")[1].split("&");
+    for (var i = 0; i < list.length; i++) {
+        var item = list[i];
+        if (item == key) {
+            return "";
+        }
+        if (item.startsWith(key + "=")) {
+            return item.replace(key + "=", "");
+        }
+    }
+    return null;
+}
+
+function isEdit() {
+    var editKey = getSearchParam("edit_key");
+    if (editKey == null) {
+        return false;
+    }
+    if (editKey == "") {
+        return false;
+    }
+    return true;
+}
+
+async function onLoad() {
+    enableLoading();
+    if (isEdit()) {
+        var editKey = getSearchParam("edit_key");
+        var queryUrl = "/delete";
+        var queryParams = {
+            edit_key: editKey;
+        };
+        try {
+            var result = await performDeleteRequest(queryUrl, queryParams);
+            if (result["status"] == "delete_ok") {
+                handleSubmissionSuccess();
+            }
+            else {
+                handleSubmissionError();
+            }
+        }
+        catch (e) {
+            handleSubmissionError();
+        }
+    }
+    else {
+        handleSubmissionError();
+    }
+    disableLoading();
+}
+
+function handleSubmissionSuccess() {
+    document.getElementById("success").classList.remove("hidden");
+}
+
+function handleSubmissionError() {
+    document.getElementById("fail").classList.remove("hidden");
+}
+
+function enableLoading() {
+}
+
+function disableLoading() {
+}
+
+onLoad();
